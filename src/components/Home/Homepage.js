e00fe694dae1398fc89742b11583919ed9545100
Pham Tuan Hiep <phamtuanhiephs@gmail.com>
2023-02-11 00:20:47 +0700

    update

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,7 +1,12 @@
+import videoHomepage from '../../assets/video-homepage.mp4'
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            home page component
+            <video autoPlay muted loop >
+                <source
+                    src={videoHomepage}
+                    type="video/mp4" />
+            </video>
         </div>
     )
 }
