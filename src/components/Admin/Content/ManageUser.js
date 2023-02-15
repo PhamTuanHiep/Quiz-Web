639e6c79619b904510e1208f4f63fd1602b35d51
Pham Tuan Hiep <phamtuanhiephs@gmail.com>
2023-02-15 20:17:45 +0700

    remakeModal

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,19 +1,31 @@
 import ModalCreateUser from "./ModalCreateUser";
 import './ManageUser.scss'
+import { FcPlus } from "react-icons/fc";
+import { useState } from "react";
+
 const ManageUser = (props) => {
+
+    const [showModalCreateUser, setShowModalCreateUser] = useState(false);
     return (
         <div className="manage-user-container">
             <div className="title">
-                Manage
-                <div className="user-content">
-                    <div>
-                        <button>Add new users</button>
-                    </div>
-                    <div>
-                        table user
-                    </div>
-                    <ModalCreateUser />
+                Manage User
+            </div>
+            <div className="user-content">
+                <div className='user-add-new'>
+                    <button
+                        className='btn btn-primary'
+                        onClick={() => setShowModalCreateUser(true)}>
+                        <FcPlus />
+                        Add new users
+                    </button>
                 </div>
+                <div className="table-users-container">
+                    table user
+                </div>
+                <ModalCreateUser
+                    show={showModalCreateUser}
+                    setShow={setShowModalCreateUser} />
             </div>
         </div>
     )
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
+import axios from 'axios';
 
-const ModalCreateUser = () => {
-    const [show, setShow] = useState(false);
+const ModalCreateUser = (props) => {
+    const { show, setShow } = props;
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => {
+        setShow(false);
+        setEmail('');
+        setPassword('');
+        setUsername('');
+        setRole('USER');
+        setImage('');
+        setPreviewImage('');
+    }
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -26,12 +34,29 @@ const ModalCreateUser = () => {
         console.log('upload file', URL.createObjectURL(event.target.files[0]))
     }
 
+    const handleSubmitCreateUser = async () => {
+        //call apis
+        // let data = {
+        //     emmail: email,
+        //     password: password,
+        //     username: username,
+        //     role: role,
+        //     userImage: image,
+        // }
+        const data = new FormData();// axios
+        data.append('email', email);
+        data.append('password', password);
+        data.append('username', username);
+        data.append('role', role);
+        data.append('userImage', image);
+
+        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
+        console.log('>>>check res: ', res)
+
+    }
+
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
-
             <Modal
                 show={show}
                 onHide={handleClose}
@@ -101,7 +126,7 @@ const ModalCreateUser = () => {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={() => handleSubmitCreateUser()}>
                         Save
                     </Button>
                 </Modal.Footer>
