3f84c820c543b3b5d2f0d9a0f474d2d0b2fe7508
Tuan Hiep <tuanhiep@gmail.com>
2023-04-02 21:58:19 +0700

    Private-route

--- src/Layout.js
+++ src/Layout.js
@@ -13,6 +13,7 @@ import ListQuiz from "./components/User/ListQuiz";
 import DetailQuiz from "./components/User/DetailQuiz";
 import ManageQuiz from "./components/Admin/Content/Quiz/ManageQuiz";
 import Questions from "./components/Admin/Content/Question/Questions";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const NotFound = () => {
   return (
@@ -27,11 +28,26 @@ const Layout = () => {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          <Route path="users" element={<ListQuiz />} />
+          <Route
+            path="users"
+            element={
+              <PrivateRoute>
+                <ListQuiz />
+              </PrivateRoute>
+            }
+          />
         </Route>
+
         <Route path="/quiz/:id" element={<DetailQuiz />} />
 
-        <Route path="admins" element={<Admin />}>
+        <Route
+          path="admins"
+          element={
+            <PrivateRoute>
+              <Admin />
+            </PrivateRoute>
+          }
+        >
           <Route index element={<Dashboard />} />
           <Route path="manage-users" element={<ManageUser />} />
           <Route path="manage-quizzes" element={<ManageQuiz />} />
@@ -40,6 +56,7 @@ const Layout = () => {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ToastContainer
